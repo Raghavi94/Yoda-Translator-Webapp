7d36d53c26f84604a6904fda55253788c5aae090
Raghavi Srinivasan <53093082+Raghavi94@users.noreply.github.com>
2021-05-05 16:10:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+var inputText = document.querySelector("#txt-ip");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputText = document.querySelector("#op");
+
+var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Something wrong with server! try again letter");
+}
+
+function clickEventHandler(){
+
+    //taking input
+
+    var txtInput = inputText.value;
+    
+    //calling server for processing
+
+    fetch(getTranslationURL(txtInput))
+        .then(response=>response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputText.innerText = translatedText; //=>output comes here
+        })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click",clickEventHandler);
